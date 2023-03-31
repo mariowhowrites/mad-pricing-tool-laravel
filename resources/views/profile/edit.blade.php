8ad5b7b26b07811f2e49322d9e0737fb604050f6
Mario Vega <mario@ceibaweb.com>
2023-03-31 16:39:45 -0700

    remove duplicate profile info partial

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -16,13 +16,7 @@
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
-
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
-            </div>
-
+            
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
